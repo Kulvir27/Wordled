a5b36997cfc73f5e1df4eab4cd1afe2df621edcc
Kulvir27 <kulvirsingh363@gmail.com>
2025-03-22 17:57:21 -0400

    updated client

--- WordleGameClient/Program.cs
+++ WordleGameClient/Program.cs
@@ -1,12 +1,37 @@
 ﻿using Grpc.Net.Client;
-// TODO: add using for gRPC service
+using WordServer;
+
 namespace WordleGameClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Connect to the service
+            var channel = GrpcChannel.ForAddress("https://localhost:7112");
+            var word = new DailyWord.DailyWordClient(channel);
+
+
+
+            // hold list of letters or dictionary to count multiple occurrences of same letter?
+
+
+
+
+
+            // TODO: game logic
+
+
         }
+
+
+
+        // function to print menu screen
+
+
     }
 }
+
+
+
