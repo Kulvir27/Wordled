ec1ac9e139dd7de3bfdc369eb4ca172a511f6df6
rdjsweet <rdjsweet@gmail.com>
2025-03-30 16:45:49 -0400

    Updated error handling, forced ports on local instances

--- WordleGameClient/Program.cs
+++ WordleGameClient/Program.cs
@@ -6,9 +6,11 @@ namespace WordleGameClient
 {
     internal class Program
     {
-        static async Task Main(string[] args) // Made async here to allow for awaits in this method
+        static async Task Main(string[] args)
         {
-            // Connect to the gRPC service
+            bool PrintStats = true;
+
+            // Server is reachable, connect to the gRPC service
             var channel = GrpcChannel.ForAddress("https://localhost:7275");
             var gameClient = new DailyWordle.DailyWordleClient(channel);
 
@@ -39,73 +41,96 @@ namespace WordleGameClient
                         continue;
                     }
 
-                    // Pass next word guessed to WordleGameService
-                    await call.RequestStream.WriteAsync(new PlayRequest { Word = guess });
-
-                    // Process the server's response
-                    if (await call.ResponseStream.MoveNext()) // Await the MoveNext() to ensure we dont access stale ResponseStream.Current
+                    try
                     {
-                        var playResponse = call.ResponseStream.Current;
+                        // Pass next word guessed to WordleGameService
+                        await call.RequestStream.WriteAsync(new PlayRequest { Word = guess });
 
-                        if (playResponse != null)
+                        // Process the server's response
+                        if (await call.ResponseStream.MoveNext()) // Await the MoveNext() to ensure we dont access stale ResponseStream.Current
                         {
-                            // If invalid guess (does not exist in wordle.json), decrement and redo turn
-                            if (!playResponse.ValidWord)
+                            var playResponse = call.ResponseStream.Current;
+
+                            if (playResponse != null)
                             {
-                                turn--; // redo turn
-                                Console.WriteLine(playResponse.Message);
-                                continue;
-                            }
+                                // If invalid guess (does not exist in wordle.json), decrement and redo turn
+                                if (!playResponse.ValidWord)
+                                {
+                                    turn--; // redo turn
+                                    Console.WriteLine(playResponse.Message);
+                                    continue;
+                                }
 
-                            // Display feedback
-                            Console.WriteLine($"     {string.Join("", playResponse.Letters.Select(l => MapFeedback(l.Feedback)))}");
-                            Console.WriteLine();
+                                // Display feedback
+                                Console.WriteLine($"     {string.Join("", playResponse.Letters.Select(l => MapFeedback(l.Feedback)))}");
+                                Console.WriteLine();
 
-                            // Update letter trackers
-                            UpdateLetterSets(playResponse.Letters, availableLetters, includedLetters, excludedLetters);
+                                // Update letter trackers
+                                UpdateLetterSets(playResponse.Letters, availableLetters, includedLetters, excludedLetters);
 
-                            // Display letter statuses
-                            Console.WriteLine($"     Included:  {string.Join(", ", includedLetters)}");
-                            Console.WriteLine($"     Available: {string.Join(", ", availableLetters)}");
-                            Console.WriteLine($"     Excluded:  {string.Join(", ", excludedLetters)}");
-                            Console.WriteLine();
+                                // Display letter statuses
+                                Console.WriteLine($"     Included:  {string.Join(", ", includedLetters)}");
+                                Console.WriteLine($"     Available: {string.Join(", ", availableLetters)}");
+                                Console.WriteLine($"     Excluded:  {string.Join(", ", excludedLetters)}");
+                                Console.WriteLine();
 
-                            // Check if the game is over
-                            if (playResponse.Correct)
-                            {
-                                Console.WriteLine(playResponse.Message);
-                                break;
-                            }
-                            else if (playResponse.GameOver)
-                            {
-                                Console.WriteLine(playResponse.Message);
-                                break;
+                                // Check if the game is over
+                                if (playResponse.Correct)
+                                {
+                                    Console.WriteLine(playResponse.Message);
+                                    break;
+                                }
+                                else if (playResponse.GameOver)
+                                {
+                                    Console.WriteLine(playResponse.Message);
+                                    break;
+                                }
                             }
                         }
+                        else
+                        {
+                            Console.WriteLine("Server ended the response stream unexpectedly.");
+                            break;
+                        }
                     }
-                    else
+                    catch (RpcException)
                     {
-                        Console.WriteLine("Server ended the response stream unexpectedly.");
+                        Console.WriteLine("\nERROR: Unable to connect to the Wordle Game Service.");
+                        Console.WriteLine("Please check your connection and try again.");
+                        PrintStats = false;
                         break;
                     }
                 }
 
-                // display statistics
-                Console.WriteLine("\nStatistics");
-                Console.WriteLine("----------");
-                StatsRequest statsRequest = new() { };
-                StatsResponse stats = gameClient.GetStats(statsRequest);
-                Console.WriteLine("Players:          " + stats.TotalPlayers);
-                Console.WriteLine("Winners:          " + Math.Round(stats.WinPercentage) + "%");
-                Console.WriteLine("Average Guesses:  " + stats.AverageGuesses);
+                // If game concludes safely
+                if (PrintStats)
+                {
+                    // Display statistics
+                    Console.WriteLine("\nStatistics");
+                    Console.WriteLine("----------");
+                    StatsRequest statsRequest = new() { };
+                    try { 
+                        StatsResponse stats = gameClient.GetStats(statsRequest);
+                        Console.WriteLine("Players:          " + stats.TotalPlayers);
+                        Console.WriteLine("Winners:          " + Math.Round(stats.WinPercentage) + "%");
+                        Console.WriteLine("Average Guesses:  " + stats.AverageGuesses);
+                    }
+                    catch (RpcException)
+                    {
+                        Console.WriteLine("ERROR: Could not retrieve statistics due to a connection error.");
+                        Console.WriteLine("Please check your connection and try again.");
+                    }
+                }
 
                 // Wrap up the game session
                 await call.RequestStream.CompleteAsync();
+                Console.WriteLine("\nPress any key to exit...");
                 Console.ReadKey();
             }
-            catch (RpcException ex)
+            catch (Exception ex)
             {
-                Console.WriteLine($"\nERROR: Unable to connect to the Wordle Game Service. Details: {ex.Message}");
+                Console.WriteLine($"\nUNEXPECTED ERROR: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
             }
         }
 
@@ -143,6 +168,7 @@ namespace WordleGameClient
 
                 switch (letterFeedback.Feedback)
                 {
+                    // Correct position and wrong position are both "included"
                     case FeedbackType.CorrectPosition:
                     case FeedbackType.WrongPosition:
                         included.Add(letter);
